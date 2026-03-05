4d2fcae48df625f37ac83b7cf6ab20ca57c073fd
linneaskold <linnea.skold@elev.ga.lbs.se>
2026-03-05 14:23:28 +0100

    Min chatbot

---
+++ Linnea chatbot, Eliza.py
@@ -0,0 +1,38 @@
+
+import random
+
+#hälsnigar
+(["hello", "hi", "hey"], [
+    "Hello!, how are you feeling today?",
+    "What brings you here to talk to me today?",
+    "Hi! tell me something thats been on your mind lately!",
+    "Good day my friend, tell me something about your day"
+    ]),
+
+ #Glad
+(["happy", "joyful", "cheerful", "delighted"], [
+    "Thats great to hear!, anything else you want to tell me?"
+    ])
+
+#husDjur
+(["dog", "cat", "hamster", "rabbit", "bunny"], [
+    "You like"
+    ])
+#kan man lägga till en joke med djuret?
+
+#utedjur
+#"Ja"
+#"Nej"
+
+
+#hejdå
+(["bye", "byee", "goodbye"],[
+    "Bye, hope you have a wonderful day!",
+    "Bye, hope we can talk to eachother again sometime",
+    "Bye, thank you for talking to me",
+    "Bye-bye, have a nice day!"
+    ]),
+
+
+
+
